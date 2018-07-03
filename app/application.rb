67afee3394b239d09a16a8c0960eaabc8c154db6
Megan Dotto <megan.dotto@gmail.com>
2018-07-03 01:04:05 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,38 @@
+class application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      find_price_return = find_price(item_name)
+
+      if find_price_return.class == String
+        resp.write find_price_return
+      else
+        resp.write "Item not found"
+        resp.status = find_price_return
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+  def find_price(item_name)
+    the_found_item = @@items.find do |e|
+      e.name == item_name
+    end
+
+    if the_found_item
+      return "#{the_found_item.price}"
+    else
+      return 400
+    end
+  end
+end
+      
